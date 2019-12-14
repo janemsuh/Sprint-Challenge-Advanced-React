b22f1fb8bb10de9751580267f2e3aa166c1b503c
Jane Suh <sourcherryyogurt@gmail.com>
2019-12-13 21:33:44 -0500

    Added light styling to player cards

--- client/src/components/PlayerData.js
+++ client/src/components/PlayerData.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 function PlayerData({ player }) {
     return (
-        <div className='player-data'>
-            <h2>{player.name}</h2>
-            <p>{player.country}</p>
-            <p>Appeared in {player.searches} queries</p>
+        <div className='player-container'>
+            <div className='player-name'>
+                <h2>{player.name}</h2>
+            </div>
+            <div className='player-info'>
+                <h3>{player.country}</h3>
+                <p>Appeared in {player.searches} queries</p>
+            </div>
         </div>
     )
 }
